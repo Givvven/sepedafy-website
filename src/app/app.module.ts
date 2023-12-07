4efa84fe8ff63f93964a312191473458c5071b26
Givvven <given.535220143@stu.untar.ac.id>
2023-12-07 22:58:28 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './pages/hero/hero.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TeamComponent } from './pages/team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    HomeComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  scrollToDiv(divId: string): void {
+    const element = document.getElementById(divId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+}
